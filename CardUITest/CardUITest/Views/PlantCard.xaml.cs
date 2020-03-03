30bf477baa3c6589cb75167d9a59d4524fc30eda
DanielSivewright <dan@wixels.com>
2020-03-03 13:05:47 +0200

    Gradient animation

--- CardUITest/CardUITest/Views/PlantCard.xaml.cs
+++ CardUITest/CardUITest/Views/PlantCard.xaml.cs
@@ -20,6 +20,7 @@ namespace CardUITest.Views
         private readonly float _cardTopMargin;
         private float _cornerRadius = 60f;
         private CardState _cardState = CardState.Collapsed;
+        private float _gradientHeight = 200f;
 
         //private static SKTypeface _typeface;
 
@@ -27,6 +28,7 @@ namespace CardUITest.Views
         SKColor _plantColor;
         SKPaint _plantPaint;
         private double _cardTopAnimPosition;
+        private float _gradientTransitionY;
 
         public PlantCard()
         {
@@ -49,6 +51,7 @@ namespace CardUITest.Views
 
             _plantColor = Color.FromHex(_viewModel.PlantColor).ToSKColor();
             _plantPaint = new SKPaint() { Color = _plantColor };
+            _gradientTransitionY = float.MaxValue;
 
             // Setup initial values
             _cardTopAnimPosition = _cardTopMargin;
@@ -77,10 +80,39 @@ namespace CardUITest.Views
 
             canvas.Clear();
 
+            // draw top hero color
             canvas.DrawRoundRect(
                 rect: new SKRect(0, (float)_cardTopAnimPosition, info.Width, info.Height),
                 r: new SKSize(_cornerRadius, _cornerRadius),
                 paint: _plantPaint);
+
+            // work out the gradient needs to be
+            var gradientRect = new SKRect(0, _gradientTransitionY, info.Width,
+                _gradientTransitionY + _gradientHeight);
+            // create the gradient
+            var gradientPaint = new SKPaint() { Style = SKPaintStyle.Fill };
+            gradientPaint.Shader = GetGradientShader(_plantColor, SKColors.White);
+
+            // draw the gradient
+            canvas.DrawRect(gradientRect, gradientPaint);
+
+            // draw the white bit
+            SKRect bottomRect = new SKRect(0, _gradientTransitionY + _gradientHeight,
+                info.Width, info.Height);
+            canvas.DrawRect(bottomRect, new SKPaint() { Color = SKColors.White });
+
+        }
+
+        private SKShader GetGradientShader(SKColor fromColor, SKColor toColor)
+        {
+            return SKShader.CreateLinearGradient
+                (
+                    start: new SKPoint(0, _gradientTransitionY),
+                    end: new SKPoint(0, _gradientTransitionY + _gradientHeight),
+                    colors: new SKColor[] { fromColor, toColor },
+                    colorPos: new float[] { 0, 1 },
+                    SKShaderTileMode.Clamp
+                );
         }
 
         private void ManagePlant_Tapped(object sender, EventArgs e)
@@ -114,16 +146,54 @@ namespace CardUITest.Views
                 parentAnimation.Add(0.10, 0.50, CreatePlantNameAnimation(cardState));
                 parentAnimation.Add(0.15, 0.50, CreatePlantTypeAnimation(cardState));
                 parentAnimation.Add(0.00, 0.25, CreateManageAnimation(cardState));
+                parentAnimation.Add(0.50, 0.75, CreateGradientAnimation(cardState));
             }
             else
             {
-                parentAnimation.Add(0.20, 0.40, CreateImageAnimation(cardState));
-                parentAnimation.Add(0.20, 0.40, CreateCardAnimation(cardState));
-                parentAnimation.Add(0.20, 0.45, CreatePlantNameAnimation(cardState));
-                parentAnimation.Add(0.20, 0.45, CreatePlantTypeAnimation(cardState));
-                parentAnimation.Add(0.30, 0.40, CreateManageAnimation(cardState));
+                parentAnimation.Add(0.25, 0.45, CreateImageAnimation(cardState));
+                parentAnimation.Add(0.25, 0.45, CreateCardAnimation(cardState));
+                parentAnimation.Add(0.25, 0.50, CreatePlantNameAnimation(cardState));
+                parentAnimation.Add(0.25, 0.50, CreatePlantTypeAnimation(cardState));
+                parentAnimation.Add(0.35, 0.45, CreateManageAnimation(cardState));
+                parentAnimation.Add(0.00, 0.25, CreateGradientAnimation(cardState));
             }
             parentAnimation.Commit(this, "CardExpand", 16, 2000);
+        }
+
+        private Animation CreateGradientAnimation(CardState cardState)
+        {
+            double start;
+            double end;
+            if (cardState == CardState.Expanded)
+            {
+                _gradientTransitionY = CardBackground.CanvasSize.Height;
+                start = _gradientTransitionY;
+                end = -_gradientHeight;
+            }
+            else
+            {
+                _gradientTransitionY = -_gradientHeight;
+                start = _gradientTransitionY;
+                end = CardBackground.CanvasSize.Height;
+            }
+
+            var gradientAnimation = new Animation(
+                callback: v =>
+                {
+                    _gradientTransitionY = (float)v;
+                    CardBackground.InvalidateSurface();
+                },
+                start: start,
+                end: end,
+                easing: Easing.Linear,
+                finished: () =>
+                {
+                    
+                }
+                );
+
+            return gradientAnimation;
+
         }
 
         private Animation CreateManageAnimation(CardState cardState)
